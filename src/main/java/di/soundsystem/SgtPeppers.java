e5974dac23091ee3f0d3ea4672079ea7bbc7d43e
RebeccaZhong <zhongyanr@126.com>
2018-04-02 06:54:46 +0800

    删掉多余代码

--- src/main/java/di/soundsystem/SgtPeppers.java
+++ src/main/java/di/soundsystem/SgtPeppers.java
@@ -16,7 +16,6 @@ public class SgtPeppers implements CompactDisc {
 	private final String artist = "astist";
 
 	public void play() {
-		// TODO Auto-generated method stub
 		System.out.println("Playing "+ title + " by " +artist);
 	}
 
