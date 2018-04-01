126380374286f04c3042368b8476a7f5e1ed820f
RebeccaZhong <zhongyanr@126.com>
2018-04-02 06:53:48 +0800

    添加注释

--- src/main/java/di/soundsystem/SgtPeppers.java
+++ src/main/java/di/soundsystem/SgtPeppers.java
@@ -3,7 +3,9 @@ package di.soundsystem;
 import org.springframework.stereotype.Component;
 
 /**
- * 此处使用@Component注解自动装配bean
+ * 此处使用@Component注解自动装配bean<br>
+ * 当@Component("lonelyHeartsClub")这样写时，即设置bean的ID为lonelyHeartsClub，
+ *   无参时默认是sgtPeppers，即类名首字母转小写
  * @author Rebecca 2018年4月2日 上午6:29:45
  * @version 1.0
  */
