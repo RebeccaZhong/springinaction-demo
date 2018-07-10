74f46139d524e3572f36e7be48c2abdd01db3d3a
zhongyan <zhongyanr@126.com>
2018-07-10 17:59:36 +0800

    添加@Autowired使用示例

--- src/main/java/di/soundsystem/CDPlayer.java
+++ src/main/java/di/soundsystem/CDPlayer.java
@@ -1,5 +1,28 @@
 package di.soundsystem;
 
-public interface CDPlayer {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer {
+	private CompactDisc cd;
+
+	@Autowired(required = false)	// 不强制注入，注意NullPointerException
+	public CDPlayer (CompactDisc cd) {
+		this.cd = cd;
+	}
+
+	public void play() {
+		cd.play();
+	}
+
+	@Autowired
+	public void setCd(CompactDisc cd) {
+		this.cd = cd;
+	}
+	@Autowired
+	public void insertDist(CompactDisc cd) {
+		this.cd = cd;
+	}
 
 }
